9b249276b0c93903b34346fab6816b96be73d8bb
Inffinite <louislaizer4@gmail.com>
2023-04-16 14:08:47 +0300

    :zap: Copy to clipboard

--- lib/widgets/AmountTag.dart
+++ lib/widgets/AmountTag.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class AmountTag extends StatelessWidget {
   final String title;
@@ -9,36 +10,63 @@ class AmountTag extends StatelessWidget {
     required this.amount,
   });
 
+  copyToClipboard(value) {
+    Clipboard.setData(
+        ClipboardData(text: '${int.parse(value.replaceAll(',', ''))}'));
+  }
+
+  void showToast(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        duration: Duration(seconds: 2),
+        backgroundColor: Color(0xff52B44B),
+        behavior: SnackBarBehavior.floating,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(
-        left: 15.0,
-        right: 15.0,
-        top: 8.0,
-        bottom: 8.0,
-      ),
-      width: double.infinity,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            title,
-            textAlign: TextAlign.start,
-            style: const TextStyle(
-                color: Color(0xff52B44B), fontSize: 14.0, fontFamily: "SFNSR"),
-          ),
-          const SizedBox(height: 5.0),
-          Text(
-            amount,
-            textAlign: TextAlign.start,
-            style: const TextStyle(
-              fontFamily: "SFT-Bold",
-              fontSize: 24.0,
-              color: Color(0xff52B44B),
+    return InkWell(
+      onTap: () {
+        copyToClipboard(amount);
+        showToast(context, "Copied to clipboard");
+      },
+      child: Container(
+        padding: const EdgeInsets.only(
+          left: 15.0,
+          right: 15.0,
+          top: 8.0,
+          bottom: 8.0,
+        ),
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              textAlign: TextAlign.start,
+              style: const TextStyle(
+                  color: Color(0xff52B44B),
+                  fontSize: 14.0,
+                  fontFamily: "SFNSR"),
             ),
-          ),
-        ],
+            const SizedBox(height: 5.0),
+            Text(
+              amount,
+              textAlign: TextAlign.start,
+              style: const TextStyle(
+                fontFamily: "SFT-Bold",
+                fontSize: 24.0,
+                color: Color(0xff52B44B),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
